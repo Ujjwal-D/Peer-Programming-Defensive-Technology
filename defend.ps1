ff4a1cba6c2c80ad15c4dddebe9f3e52f37faaf4
Ujjwal Dhakal <70379407+Ujjwal-D@users.noreply.github.com>
2024-04-18 11:04:01 +1000

    Added defend.ps1 to the repo
    Committed script defend.ps1 which tries to access an unauthorised app ' keyfinder.exe '

---
+++ defend.ps1
@@ -0,0 +1,22 @@
+﻿function testWDAC
+{ 
+    # unauthorized file.
+   
+    Start-Process -FilePath "C:\keyfinder.exe" -Wait
+    $NameofApp = "keyfinder.exe"
+
+    $Runs = Get-Process | Where-Object { $_.ProcessName -eq $NameofApp }
+
+    if ($Runs) 
+    {
+        Write-Host "$NameofApp runs which means WDAC is blocking this app."
+
+    }
+    else 
+    {
+        Write-Host "$NameofApp does not run which means WDAC is blocking this app."
+
+    }
+} 
+Invoke-ScriptAnalyzer defend.ps1
+testWDAC
