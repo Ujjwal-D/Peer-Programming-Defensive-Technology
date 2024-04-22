0f69c102af7afe4689e667f0279ae586fefd3de9
tanisha2002oishi <165628932+tanisha2002oishi@users.noreply.github.com>
2024-04-22 14:32:59 +1000

    Add files via upload

---
+++ SetupWDAC.ps1
@@ -0,0 +1,17 @@
+﻿## Reference: https://learn.microsoft.com/en-us/windows/security/application-security/application-control/windows-defender-application-control/deployment/wdac-deployment-guide
+
+## Update the path to your WDAC policy XML
+ $WDACPolicyXMLFile = $env:USERPROFILE + "\Desktop\MyWDACPolicy.xml"
+ [xml]$WDACPolicy = Get-Content -Path $WDACPolicyXMLFile
+ if (($WDACPolicy.SiPolicy.PolicyID) -ne $null) ## Multiple policy format (For Windows builds 1903+ only, including Server 2022)
+ {
+     $PolicyID = $WDACPolicy.SiPolicy.PolicyID
+     $PolicyBinary = $PolicyID+".cip"
+ }
+ else ## Single policy format (Windows Server 2016 and 2019, and Windows 10 1809 LTSC)
+ {
+     $PolicyBinary = "SiPolicy.p7b"
+ }
+ 
+ ## Binary file will be written to your desktop
+ ConvertFrom-CIPolicy -XmlFilePath $WDACPolicyXMLFile -BinaryFilePath $env:USERPROFILE\Desktop\$PolicyBinary
---
+++ enableWDAC.ps1
@@ -0,0 +1,14 @@
+﻿
+# Path to the policy binary file
+$PolicyBinary =  "C:\Users\vagrant\Desktop\{A244370E-44C9-4C06-B551-F6016E563076}.cip"
+
+# Destination folder for the policy binary file
+$DestinationFolder = $env:windir + "\System32\CodeIntegrity\CIPolicies\Active\"
+
+# Copy the policy binary file to the destination folder
+Copy-Item -Path $PolicyBinary -Destination $DestinationFolder -Force
+
+# Use gpupdate to refresh policy settings
+Write-Host "Refreshing Group Policy settings..."
+gpupdate /force
+Write-Host "Group Policy settings refreshed."
