948d3d93b10ebd6ad66037b4eb23e10625caf23b
tanisha2002oishi <165628932+tanisha2002oishi@users.noreply.github.com>
2024-04-24 01:12:01 +1000

    Add files via upload

--- enableWDAC.ps1
+++ enableWDAC.ps1
@@ -1,5 +1,7 @@
-﻿
+﻿#### Reference:https://learn.microsoft.com/en-us/windows/security/application-security/application-control/windows-defender-application-control/deployment/deploy-wdac-policies-with-script
 # Path to the policy binary file
+function enableWDAC{
+
 $PolicyBinary =  "C:\Users\vagrant\Desktop\{A244370E-44C9-4C06-B551-F6016E563076}.cip"
 
 # Destination folder for the policy binary file
@@ -12,3 +14,6 @@ Copy-Item -Path $PolicyBinary -Destination $DestinationFolder -Force
 Write-Host "Refreshing Group Policy settings..."
 gpupdate /force
 Write-Host "Group Policy settings refreshed."
+}
+
+enableWDAC
