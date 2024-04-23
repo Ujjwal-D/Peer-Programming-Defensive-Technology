25fab1e590a247e9448fb0e6cdc2f62edb2c21bc
Ujjwal Dhakal <70379407+Ujjwal-D@users.noreply.github.com>
2024-04-23 21:26:33 +1000

    Adding resetWDAC file to reset the WDAC Policy

---
+++ resetWDAC.ps1
@@ -0,0 +1,20 @@
+﻿function resetWDAC {
+        
+        # Getting the Policy GUID to reset the WDAC block policy
+      $PolicyXMLFile = "C:\Users\vagrant\Desktop\MyWDACPolicy.xml"
+      [xml]$WDACPolicy = Get-Content -Path $PolicyXMLFile
+
+      if(($WDACPolicy.SiPolicy.PolicyID) -ne $null)
+      {
+        $PolicyID = $WDACPolicy.SiPolicy.PolicyID
+      }
+
+      # Removing the policy.
+      
+      Remove-Item -Path "C:\Windows\System32\CodeIntegrity\CiPolicies\Active\$PolicyID.cip"
+       Write-Host "Resetting WDAC Policy .........."
+       Write-Host "Restarting Computer............"
+      Restart-Computer
+    }
+    # calling the function.
+    resetWDAC
